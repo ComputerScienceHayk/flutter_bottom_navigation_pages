8e1f8551e014a0a5138e4050f7b02857f33f9064
ComputerScienceHayk <59318612+ComputerScienceHayk@users.noreply.github.com>
2020-07-03 04:07:52 +0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -20,19 +20,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
-
-//import 'package:flutter/material.dart';
-//import 'package:test_all/home_widget.dart';
-//
-//void main() => runApp(App());
-//
-//class App extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    return MaterialApp(
-//      title: 'My Flutter App',
-//      home: Home(),
-//    );
-//  }
-//}
